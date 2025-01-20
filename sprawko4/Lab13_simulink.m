d43ae580b60ee8c777dfc634faf413485249a3a2
Igor <109353344+Hajvur@users.noreply.github.com>
2025-01-20 12:10:34 +0100

    added tests for parameters

--- sprawko4/Lab13_simulink.m
+++ sprawko4/Lab13_simulink.m
@@ -1,23 +1,50 @@
 close all; clear; clc;
 
 global kp ti
+iter = [5 10 15];
+%max iter
+for i = 1:size(iter)
+    % PSO
+    opt = optimoptions("particleswarm", 'PlotFcn', @pswplotbestf);
+    opt.MaxIterations = iter(i);
+    opt.SwarmSize = 10;
 
+    [wartosci_obliczone, wartosc_f_pso] = particleswarm(@flc_error_ffunc, 2, [1 0.001], [100 3], opt);
+end
 
-% PSO
-opt = optimoptions("particleswarm", 'PlotFcn', @pswplotbestf);
-opt.MaxIterations = 5;
-opt.SwarmSize = 10;
+swarm_size = [10 20 50];
+% swarm size
+for i = 1:size(swarm_size)
+    % PSO
+    opt = optimoptions("particleswarm", 'PlotFcn', @pswplotbestf);
+    opt.MaxIterations = 5;
+    opt.SwarmSize = swarm_size(i);
 
-[wartosci_obliczone, wartosc_f_pso] = particleswarm(@flc_error_ffunc, 2, [1 0.001], [100 3], opt);
-
-
+    [wartosci_obliczone, wartosc_f_pso] = particleswarm(@flc_error_ffunc, 2, [1 0.001], [100 3], opt);
+end
 
 %% GA
 
 global kp ti
 
-opt = optimoptions("ga", 'PlotFcn', @gaplotbestf);
-opt.MaxGenerations = 5;
-opt.PopulationSize = 10;
+iter = [5 10 15];
 
-[wartosci_wyznaczone, wartosc_f_ga] = ga(@flc_error_ffunc, 2, [],[],[],[], [1 0.001], [100 3], [], opt);
+for i = 1:size(iter)
+
+    opt = optimoptions("ga", 'PlotFcn', @gaplotbestf);
+    opt.MaxGenerations = iter(i);
+    opt.PopulationSize = 10;
+
+    [wartosci_wyznaczone, wartosc_f_ga] = ga(@flc_error_ffunc, 2, [], [], [], [], [1 0.001], [100 3], [], opt);
+end
+
+popul_size = [5 10 15];
+
+for i = 1:size(iter)
+
+    opt = optimoptions("ga", 'PlotFcn', @gaplotbestf);
+    opt.MaxGenerations = 5;
+    opt.PopulationSize = popul_size(i);
+
+    [wartosci_wyznaczone, wartosc_f_ga] = ga(@flc_error_ffunc, 2, [], [], [], [], [1 0.001], [100 3], [], opt);
+end

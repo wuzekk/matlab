21e2f78230f3f92f1a66e4034b197e8d15b563f5
Igor <109353344+Hajvur@users.noreply.github.com>
2025-01-24 13:15:25 +0100

    added parameters

--- sprawko4/pi_regulator_ga_pso.m
+++ sprawko4/pi_regulator_ga_pso.m
@@ -6,7 +6,7 @@ global kp ti
 %ga
 opt = optimoptions("ga", 'PlotFcn', @gaplotbestf);
 opt.MaxGenerations = 5;
-opt.PopulationSize = 10;
+opt.PopulationSize = 50;
 
 [wartosci_wyznaczone, wartosc_f_ga] = ga(@flc_error_ffunc, 2, [], [], [], [], [1 0.001], [100 3], [], opt);
 
@@ -20,12 +20,14 @@ xlabel('t [s]');
 ylabel('\omega [p.u.]');
 legend('init', 'optim');
 title(['GA Optimization Result: ', num2str(wartosc_f_ga)]);
+data = {'kp', kp; 'ti', ti; 'Max Generations', opt.MaxGenerations; 'Population Size', opt.PopulationSize};
+uitable('Data', data, 'ColumnName', {'Parameter', 'Value'}, 'Position', [350 120 200 100]);
 grid on;
 
 %pso
 opt = optimoptions("particleswarm", 'PlotFcn', @pswplotbestf);
 opt.MaxIterations = 5;
-opt.SwarmSize = 10;
+opt.SwarmSize = 50;
 
 [wartosci_obliczone, wartosc_f_pso] = particleswarm(@flc_error_ffunc, 2, [1 0.001], [100 3], opt);
 
@@ -40,3 +42,5 @@ xlabel('t [s]');
 ylabel('\omega [p.u.]');
 legend('init', 'optim');
 title(['PSO Optimization Result: ', num2str(wartosc_f_pso)]);
+data = {'kp', kp; 'ti', ti; 'Max Iteration', opt.MaxIterations; 'Swarm Size', opt.SwarmSize};
+uitable('Data', data, 'ColumnName', {'Parameter', 'Value'}, 'Position', [350 120 200 100]);
